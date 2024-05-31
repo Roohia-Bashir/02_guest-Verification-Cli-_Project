fd2607bfa6eaf5af20eba7c87bc354d2d04a339b
Roohia-Bashir <160015350+Roohia-Bashir@users.noreply.github.com>
2024-05-31 04:01:15 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# 02_guest-Verification-Cli-_Project
-This project will verify if user's entered name is present in today invited guests.
+Project Description:
+
+This is a guest verification CLI project. It's designed to facilitate guest management for events, conferences, or any gathering where guest verification is required.
+
+Installation:
+
+To install and use this CLI tool, simply run the following command:
+
+Copy code
+
+npm i rs265_guest-verification-cli_project
+npx rs265_guest-verification-cli_project
---
+++ index.ts
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.green(`Welcome to Guest Verification Center`));
+let myLoop = true;
+let myInvitedGuestList = ['hina', 'ruhi', 'sidra', 'maham', 'rabia', 'anila', 'ayesha', 'hoorain'];
+
+while(myLoop){
+
+    let userInput = await inquirer.prompt({
+        type: 'input',
+        name: 'userName',
+        message: 'please enter your name:'
+    });
+
+let guestName = userInput.userName;
+// let {userName} = userInput; // destructuring
+//console.log(guestName);
+let lowerGuestName = guestName.toLowerCase();
+
+if(myInvitedGuestList.includes(lowerGuestName)){
+    console.log(chalk.yellow(`Welcome Miss ${guestName}! please, make yourself comfortable.`));
+    console.log("Thank you!");
+myLoop = false
+
+}else{
+    console.log(chalk.red(`\nSorry, Miss ${guestName}. Your name is not on the guest list for today.\n`));
+ 
+let askNameAgain = await inquirer.prompt({
+    type: 'confirm',
+    name: 'otherName',
+    message: 'Do you have another name you go by? if so, we can check again!',
+    default: false
+})
+if(askNameAgain.otherName == false){
+    myLoop = false,
+    console.log(chalk.magenta(`\nWe apologize, but you are not on the guest list. Please contact the event organizer.\n`));   
+}  
+}
+}
